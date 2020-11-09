aab92d4528b042f3017f39f2654e0a30f942cdad
TJ-Dewey <63527902+TJ-Dewey@users.noreply.github.com>
2020-11-08 18:10:16 -0800

    Update JobHunter.py

--- JobHunter/JobHunter.py
+++ JobHunter/JobHunter.py
@@ -18,12 +18,12 @@ def connect_to_sql():
     return conn
 
 # Create the table structure
-def create_tables(cursor, table):
+def create_tables(cursor):
     ## Add your code here. Starter code below
     cursor.execute('''CREATE TABLE IF NOT EXISTS Jobs_found (id INT PRIMARY KEY auto_increment,
                         Type varchar(10), Title varchar(100), Description TEXT, Job_id varchar(33),
                         Created_at DATE, Company varchar(100), location varchar(50),
-                        How_to_apply varchar(1000)); ''')
+                        How_to_apply varchar(1000));''')
     return
 
 # Query the database.
@@ -41,12 +41,30 @@ def add_new_job(cursor, jobdetails):
 # Check if new job
 def check_if_job_exists(cursor, jobdetails):
     ## Add your code here
-    query = "SELECT"
+    type = jobdetails['type']
+    title = jobdetails['title']
+    description = jobdetails['description']
+    job_id = jobdetails['id']
+    created_at = time.strptime(jobdetails['created_at'], "%a %b %c %H:%M:%S %Z %Y")
+    company = jobdetails['company']
+    location = jobdetails['location']
+    how_to_apply = jobdetails['how_to_apply']
+    query = (INSERT INTO jobs (catagories) VALUES(%s, %s, %s, %s, %s, %s, %s, %s,)
+                           (type, title, description, job_id, created_at, company,
+                            location, how_to_apply))
+    return query_sql(cursor, query)
+
+# Check if new job
+def check_if_job_exists(cursor, jobdetails, table="Jobs_found"):
+    ## Add your code here
+    job_id = jobdetails['id']
+    query = "SELECT * FROM jobs WHERE Job_id = \'%s\'" % job_id
     return query_sql(cursor, query)
 
 def delete_job(cursor, jobdetails):
     ## Add your code here
-    query = "UPDATE"
+    job_id = Jobdetails['id']
+    query = "DELETE FROM jobs WHERE job_id = \'%s\'" % job_id
     return query_sql(cursor, query)
 
 # Grab new jobs from a website
@@ -82,16 +100,24 @@ def load_config_file(filename):
     return argument_dictionary
 
 # Main area of the code.
-def jobhunt(cursor, arg_dict):
+def jobhunt(arg_dict, cursor):
     # Fetch jobs from website
     jobpage = fetch_new_jobs(arg_dict)
-    # print (jobpage)
+    #! print (jobpage) #?suggested or required?
     ## Add your code here to parse the job page
-
+        #? parse arg_dict into 'job details" var here?
+    add_or_delete_job(jobpage, cursor)
+def add_or_delete_job(jobpage, cursor):
     ## Add in your code here to check if the job already exists in the DB
-
-    ## Add in your code here to notify the user of a new posting
-
+    for detail in jobpage:
+        check_if_job_exists(cursor, details)
+        is_job_found = len(cursor.fetchall()) > 0
+        ## Add in your code here to notify the user of a new posting
+        if is_job_found:
+            print("Last job found: " + jobdetails['title'] + " from " + jobdetails["company"])
+        else:
+            print("New job found:" + jobdetails['title'] + " from " + jobdetails["company"])
+            add_new_job(cursor, jobdetails)
     ## EXTRA CREDIT: Add your code to delete old entries
 
 # Setup portion of the program. Take arguments and set up the script
@@ -100,7 +126,7 @@ def main():
     # Connect to SQL and get cursor
     conn = connect_to_sql()
     cursor = conn.cursor()
-    create_tables(cursor, "table")
+    create_tables(cursor)
     # Load text file and store arguments into dictionary
     arg_dict = load_config_file(sys.argv[1])
     while(1):
